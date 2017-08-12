36fb1a584d3f619efc071bd145f3b3bd97dda171
Annie <ananya.s.a@gmail.com>
2017-08-12 10:37:22 -0700

    Add prime folder and prime.c

---
+++ prime/prime.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, i, flag = 0;
+
+	printf("Enter int");
+	scanf("%d", &n);
+
+	for(i = 2; i <= n/2; ++i)
+	{
+		if(n%i == 0)
+		{
+			flag = 1;
+			break;
+		}
+	}
+
+	if(flag == 0)
+		printf("Is a prime");
+	else
+		printf("Is not a prime");
+
+	return 0;
+}
